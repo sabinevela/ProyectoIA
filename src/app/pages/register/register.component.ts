8fa76d988a5ba84a72d0b55db5f5acaba59470e4
sabinevela <srjsab5@gmail.com>
2025-08-07 08:21:21 -0500

    subido

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,        
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
@@ -22,8 +26,21 @@ export class RegisterComponent {
   name = '';
   email = '';
   password = '';
+  hidePassword = true;
+  isLoading = false;
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
 
   onSubmit() {
-    alert(`Registro simulado para ${this.name}`);
+    if (!this.name || !this.email || !this.password) return;
+
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.isLoading = false;
+      alert(`Registro exitoso de ${this.name}`);
+    }, 2000);
   }
 }
